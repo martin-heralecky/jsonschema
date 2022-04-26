d8a815de42264edafb1921ef5b4ab075f23ab486
Martin Heralecký <heralecky.martin@gmail.com>
2022-04-26 14:31:06 +0200

    add Value wrapper

---
+++ src/Value.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace MartinHeralecky\Jsonschema;
+
+/**
+ * Wrapper for any value. Useful in situations, in which `null` is treated both as a legal value and as an indicator
+ * that something doesn't exist.
+ *
+ * @template T
+ */
+class Value
+{
+    /**
+     * @param T $value
+     */
+    public function __construct(
+        private mixed $value,
+    ) {
+    }
+
+    /**
+     * @return T
+     */
+    public function getValue(): mixed
+    {
+        return $this->value;
+    }
+}
