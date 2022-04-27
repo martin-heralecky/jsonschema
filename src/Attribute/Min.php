f9e3a73a0d1206c31cfd144ca6c8efa884b16de3
Martin Heralecký <heralecky.martin@gmail.com>
2022-04-27 14:28:46 +0200

    Min and Max attributes

---
+++ src/Attribute/Min.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace MartinHeralecky\Jsonschema\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class Min
+{
+    public function __construct(
+        private int|float $value,
+    ) {
+    }
+
+    public function getValue(): int|float
+    {
+        return $this->value;
+    }
+}
