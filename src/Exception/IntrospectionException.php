12228e138e2ee228b46bc21a59d3a4024a20e9d7
Martin Heralecký <heralecky.martin@gmail.com>
2022-05-05 14:20:53 +0200

    add exceptions

---
+++ src/Exception/IntrospectionException.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace MartinHeralecky\Jsonschema\Exception;
+
+class IntrospectionException extends JsonschemaException
+{
+}
---
+++ src/Exception/JsonschemaException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace MartinHeralecky\Jsonschema\Exception;
+
+use Exception;
+
+class JsonschemaException extends Exception
+{
+}
