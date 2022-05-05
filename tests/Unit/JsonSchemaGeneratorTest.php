e8a389f126242cc1338b7a019d9e8081f9507177
Martin Heralecký <heralecky.martin@gmail.com>
2022-05-05 13:53:33 +0200

    fix JsonSchemaGeneratorTest

--- tests/Unit/JsonSchemaGeneratorTest.php
+++ tests/Unit/JsonSchemaGeneratorTest.php
@@ -3,11 +3,11 @@
 namespace MartinHeralecky\Jsonschema\Tests\Unit;
 
 use MartinHeralecky\Jsonschema\JsonSchemaGenerator;
-use MartinHeralecky\Jsonschema\Schema\IntegerValue;
-use MartinHeralecky\Jsonschema\Schema\ObjectValue;
-use MartinHeralecky\Jsonschema\Schema\ObjectValueProperty;
-use MartinHeralecky\Jsonschema\Schema\Schema;
-use MartinHeralecky\Jsonschema\Schema\StringValue;
+use MartinHeralecky\Jsonschema\Schema\IntegerSchema;
+use MartinHeralecky\Jsonschema\Schema\ObjectSchema;
+use MartinHeralecky\Jsonschema\Schema\ObjectSchemaProperty;
+use MartinHeralecky\Jsonschema\Schema\StringSchema;
+use MartinHeralecky\Jsonschema\Value;
 use PHPUnit\Framework\TestCase;
 
 class JsonSchemaGeneratorTest extends TestCase
@@ -19,57 +19,55 @@ class JsonSchemaGeneratorTest extends TestCase
         $this->gen = new JsonSchemaGenerator();
     }
 
-    public function testSchema(): void
+    public function testIntegerSchema(): void
     {
-        $json = $this->gen->generate(new Schema(new IntegerValue(), "My Title"));
-
-        $this->assertSame("My Title", $json["title"]);
-    }
-
-    public function testIntegerValue(): void
-    {
-        $val  = new IntegerValue("My description.", 3, [1, 2], -5, 5);
-        $json = $this->gen->generate(new Schema($val));
+        $schema = new IntegerSchema("My Title", "My description.", new Value(3), [1, 2], [], -5, 5);
+        $json   = $this->gen->generate($schema);
 
         $this->assertSame("integer", $json["type"]);
+        $this->assertSame("My Title", $json["title"]);
         $this->assertSame("My description.", $json["description"]);
         $this->assertSame(3, $json["default"]);
         $this->assertSame(1, $json["examples"][0]);
         $this->assertSame(2, $json["examples"][1]);
         $this->assertSame(-5, $json["minimum"]);
         $this->assertSame(5, $json["maximum"]);
+        $this->assertArrayNotHasKey("enum", $json);
     }
 
-    public function testStringValue(): void
+    public function testStringSchema(): void
     {
-        $val  = new StringValue("My description.", "foo", ["bar", "gee"], null);
-        $json = $this->gen->generate(new Schema($val));
+        $schema = new StringSchema("My Title", "My description.", new Value("foo"), ["bar", "gee"], [], null);
+        $json   = $this->gen->generate($schema);
 
         $this->assertSame("string", $json["type"]);
+        $this->assertSame("My Title", $json["title"]);
         $this->assertSame("My description.", $json["description"]);
         $this->assertSame("foo", $json["default"]);
         $this->assertSame("bar", $json["examples"][0]);
         $this->assertSame("gee", $json["examples"][1]);
+        $this->assertArrayNotHasKey("enum", $json);
         $this->assertArrayNotHasKey("pattern", $json);
     }
 
-    public function testObjectValue(): void
+    public function testObjectSchema(): void
     {
-        $val  = new ObjectValue("My description.", [
-            new ObjectValueProperty("foo", true, new IntegerValue()),
-            new ObjectValueProperty("bar", false, new ObjectValue("Another description.", [
-                new ObjectValueProperty("gee", true, new StringValue()),
+        $schema = new ObjectSchema("My Title", "My description.", [
+            new ObjectSchemaProperty("alfa", new IntegerSchema(null, null, new Value(10))),
+            new ObjectSchemaProperty("bravo", new ObjectSchema("Another Title", "Another description.", [
+                new ObjectSchemaProperty("charlie", new StringSchema(null, null, new Value("def"))),
             ])),
         ]);
-        $json = $this->gen->generate(new Schema($val));
+        $json   = $this->gen->generate($schema);
 
         $this->assertSame("object", $json["type"]);
+        $this->assertSame("My Title", $json["title"]);
         $this->assertSame("My description.", $json["description"]);
-        $this->assertSame("integer", $json["properties"]["foo"]["type"]);
-        $this->assertSame("object", $json["properties"]["bar"]["type"]);
-        $this->assertSame("Another description.", $json["properties"]["bar"]["description"]);
-        $this->assertSame("string", $json["properties"]["bar"]["properties"]["gee"]["type"]);
-        $this->assertSame(["gee"], $json["properties"]["bar"]["required"]);
-        $this->assertSame(["foo"], $json["required"]);
+        $this->assertSame("integer", $json["properties"]["alfa"]["type"]);
+        $this->assertSame("object", $json["properties"]["bravo"]["type"]);
+        $this->assertSame("Another Title", $json["properties"]["bravo"]["title"]);
+        $this->assertSame("Another description.", $json["properties"]["bravo"]["description"]);
+        $this->assertSame("string", $json["properties"]["bravo"]["properties"]["charlie"]["type"]);
+        $this->assertSame(["bravo"], $json["required"]);
     }
 }
