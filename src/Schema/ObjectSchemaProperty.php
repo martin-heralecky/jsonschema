be6d37187c9ec628ae0616e9359a404811dc44bf
Martin Heralecký <heralecky.martin@gmail.com>
2023-04-13 16:55:33 +0200

    objectschemaproperty add fields

--- src/Schema/ObjectSchemaProperty.php
+++ src/Schema/ObjectSchemaProperty.php
@@ -2,12 +2,22 @@
 
 namespace MartinHeralecky\Jsonschema\Schema;
 
+use MartinHeralecky\Jsonschema\Cast\JsonToPhpCast;
+use MartinHeralecky\Jsonschema\Cast\PhpToJsonCast;
+use MartinHeralecky\Jsonschema\Value;
+
 class ObjectSchemaProperty
 {
     public function __construct(
         private readonly string $name,
         private readonly string $phpName,
         private readonly Schema $schema,
+        private readonly ?string $description = null,
+        private readonly ?Value $default = null,
+        private readonly array $examples = [],
+        private readonly array $enumValues = [],
+        private readonly ?JsonToPhpCast $jsonToPhpCast = null,
+        private readonly ?PhpToJsonCast $phpToJsonCast = null,
     ) {
     }
 
@@ -24,5 +34,35 @@ class ObjectSchemaProperty
     public function getSchema(): Schema
     {
         return $this->schema;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function getDefault(): ?Value
+    {
+        return $this->default;
+    }
+
+    public function getExamples(): array
+    {
+        return $this->examples;
+    }
+
+    public function getEnumValues(): array
+    {
+        return $this->enumValues;
+    }
+
+    public function getJsonToPhpCast(): ?JsonToPhpCast
+    {
+        return $this->jsonToPhpCast;
+    }
+
+    public function getPhpToJsonCast(): ?PhpToJsonCast
+    {
+        return $this->phpToJsonCast;
     }
 }
